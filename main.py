cd85c0b4f9876277c86322f2db4bc2426b89dde4
Punit1511 <92090365+Punit1511@users.noreply.github.com>
2021-10-08 11:50:46 +0530

    I have made changes to delete and update method

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ class Worker:
     worker_list = []
 
     def Add_Employ(self):
-        self.name = input("Enter Employ Name  : ")
-        self.gender = input("Enter The Gender : ")
-        self.city = input("Enter The City of Worker : ")
-        self.age = int(input("Enter the Age of a Worker : "))
-        self.dob = (input("Enter The Date of Birth Of a Worker in DD-MM-YYYY :  "))
-        self.address = input("Enter The Proper Address with Pin Cod e of a Worker : ")
-        self.qualification = input("Enter The highest qualification of a worker : ")
-        self.designation = input("Enter Employ Post : ")
-        self.salary = int(input("Enter Employ Salary : "))
+        self.name = input("Enter Employ Name  : \n")
+        self.gender = input("Enter The Gender : \n")
+        self.city = input("Enter The City of Worker : \n")
+        self.age = int(input("Enter the Age of a Worker : \n"))
+        self.dob = (input("Enter The Date of Birth Of a Worker in DD-MM-YYYY :  \n"))
+        self.address = input("Enter The Proper Address with Pin Cod e of a Worker : \n")
+        self.qualification = input("Enter The highest qualification of a worker : \n")
+        self.designation = input("Enter Employ Post : \n")
+        self.salary = int(input("Enter Employ Salary : \n"))
         Worker.worker_id += 1
         Worker.worker_count += 1
         print("Employ Added Successfully")
@@ -29,22 +29,122 @@ class Worker:
             else:
                 print("Data not found against the search")
 
-
-
     def update_worker_data(self):
-        self.new_name = input("Enter The worker name again : ")
-        self.new_gender = input("Enter The Gender again : ")
-        self.new_city = input("Enter The City of Worker again : ")
-        self.new_age = int(input("Enter the Age of a Worker again : "))
-        self.new_dob = (input("Enter The Date of Birth Of a Worker in DD-MM-YYYY again :  "))
-        self.new_address = input("Enter The Proper Address with Pin Cod e of a Worker again : ")
-        self.new_qualification = input("Enter The highest qualification of a worker again : ")
-        self.new_designation = input("Enter Employ Post again : ")
-        self.new_salary = int(input("Enter Employ Salary again : "))
+        file=open('Workerinformation.csv', 'r')
+        Reader = csv.reader(file)
+        Update_worker = []
+        while True:
+            try:
+                Uworker_id = int(input('Enter the id of worker whose information you want to update'))
+            except ValueError:
+                print("Please Enter a Valid worker Id")
+                continue
+            else:
+                break
+        Found = False
+        for row in Reader:
+            if row[0] == str(Uworker_id):
+                Found = True
+                while True:
+                    try:
+                        choice_for_update = int(input("Which Information you want to update : \n" 
+                                                      "\n1) Name" 
+                                                      "\n2) Gender" 
+                                                      "\n3) City" 
+                                                      "\n4) Age" 
+                                                      "\n5) Date Of Birth" 
+                                                      "\n6) Address" 
+                                                      "\n7) Qulification" 
+                                                      "\n8) Designation" 
+                                                      "\n 9) Salary \n"))
+                    except ValueError:
+                        print("Sorry I dont understand you choice")
+                        continue
+                    else:
+                        break
+                if choice_for_update == 1:
+                    name = input("Enter the new Name you want to update \n")
+                    row[1] = name
+                    Update_worker.append(row)
+
+                elif choice_for_update == 2:
+                    Gender = input("Enter the Gender Again \n")
+                    row[2] = Gender
+                    Update_worker.append(row)
+
+                elif choice_for_update == 3:
+                    City = input("Enter the new City of Worker \n")
+                    row[3] = City
+                    Update_worker.append(row)
+
+                elif choice_for_update == 4:
+                    Age = input("Enter The new Age of Worker \n")
+                    row[4] = Age
+                    Update_worker.append(row)
+
+                elif choice_for_update == 5:
+                    Date_Of_Birth = input("Enter the new date of birth of Worker \n")
+                    row[5] = Date_Of_Birth
+                    Update_worker.append(row)
+
+                elif choice_for_update == 6:
+                    Address = input("Enter the new Address of Worker \n")
+                    row[6] = Address
+                    Update_worker.append(row)
+
+                elif choice_for_update == 7:
+                    Qualification = input("Enter the new qualification of a worker \n")
+                    row[7] = Qualification
+                    Update_worker.append(row)
+                elif choice_for_update == 8:
+                    Designation = input("Enter the new Designation of a Worker \n")
+                    row[8] = Designation
+                    Update_worker.append(row)
+                elif choice_for_update == 9:
+                    Salary = input("Enter the new Salary of a Worker \n")
+        file.close()
+        if Found == False:
+            print('Worker Not Found')
+        else:
+            file = open('Workerinformation.csv', 'w+', newline='')
+            writer = csv.writer(file)
+            writer.writerows(Update_worker)
+            file.seek(0)
+            for row in Reader :
+                print(row)
+            file.close
+
+    def Delete_Worker_Information(self):
+        files = open('Workerinformation.csv', 'r')
+        Reader = csv.reader(files)
+        delete_worker = []
+        while True:
+            try:
+                Dworker = int(input("Enter Worker Id to be deleted"))
+            except ValueError:
+                print("Please Enter Valid Worker Id")
+                continue
+            else:
+                break
+        Found_delete = False
+        for row in Reader:
+            if row[0] == str(id):
+                Found = True
+            else:
+                delete_worker.append(row)
+        file.close()
+        if Found_delete == False:
+            print('Worker not Found to Delete')
+        else:
+            files = open('Workerinformation.csv', 'w+', newline='')
+            writer = csv.writer(files)
+            file.seek(0)
+            Reader = csv.reader(files)
+            for row in Reader :
+                print(row)
+            file.close()
 
 
-    def updated_details(self):
-        print('|', self.new_name, '|', self.new_gender, '  |', self.new_city, '|', self.new_age, ' |', self.new_dob, '   |', self.new_address,' |', self.new_qualification, '     |', self.new_designation, ' |', self.new_salary, '|')
 
 
 
@@ -56,8 +156,8 @@ while True:
                 choice_of_user = int(input("PLEASE SELECT ANY ONE CHOICE FROM BELOW:" 
                                "\n1) INSERT INFORMATION OF WORKERS "
                                "\n2) SEARCH INFORMATION OF WORKERS " 
-                               "\n4) UPDATE INFORMATION OF WORKER "
-                               "\n5) DELETE THE INFORMATION OF WORKER \n"))
+                               "\n3) UPDATE INFORMATION OF WORKER "
+                               "\n4) DELETE THE INFORMATION OF WORKER \n"))
             except ValueError:
                 print("Sorry I dont understand your answer ")
                 continue
@@ -84,7 +184,7 @@ while True:
                 print(workerobj.salary)
                 print(workerobj.worker_count)
                 with open('Workerinformation.csv', 'w+') as file:
-                    myFile = CSV.Writer(file)
+                    myFile = csv.writer(file)
                     myFile.writerow(["id", "Name", "Gendr", "City", "Age", "Date Of Birth", "Address", "Qulification", "Designation", "Salary"])
                     workerid = (workerobj.worker_id)
                     WorkerName = (workerobj.name)
@@ -93,7 +193,10 @@ while True:
                     WorkerAge = (workerobj.age)
                     WorkerDate_Of_Birth = (workerobj.dob)
                     WorkerAddress = (workerobj.address)
-                    WorkerQulificat
+                    WorkerQulificat = (workerobj.qualification)
+                    WorkerDesignation = (workerobj.designation)
+                    WorkerSalary = (workerobj.salary)
+                    myFile.writerow([workerid, WorkerName, WorkerGender, WorkerCity, WorkerAge, WorkerDate_Of_Birth, WorkerAddress, WorkerQulificat, WorkerDesignation, WorkerSalary])
 
 
                 choice_for_display = input("You Want to see the data which is inserted y or n ")
@@ -101,39 +204,37 @@ while True:
                     print('|', 'NAME','  |','GENDER','|','CITY','  |','AGE','|','DATE OF BIRTH','|','ADDRESS','        |','QUALIFICATION','|','DESIGNATION','|','SALARY','|')
                     for w1 in Worker.worker_list:
                         w1.display_details()
-            choice = input("If you want To stop to insert press yes otherwise press any key ")
+            choice = input("If you want To stop to insert press yes otherwise press any key\n")
             if choice == 'yes' or choice == 'YES':
                 break
     elif choice_of_user == 2:
             w1 = Worker()
             data_searched = w1.search_data()
-            choice_for_search = ("DO you want to search more information ? If Yes press Y or y else press any key ")
+            choice_for_search = ("DO you want to search more information ? If Yes press Y or y else press any key\n")
             if choice_for_search == 'y' or choice_for_search == 'Y' :
                 break
 
-    elif choice_of_user == 4:
+    elif choice_of_user == 3:
         while True:
             w1 = Worker()
             w1.update_worker_data()
-            for data_searched in Worker.worker_list:
-                Worker.worker_list.append(w1)
-                w1.updated_details()
-                choice_for_update =()
-
-
-    elif choice_of_user == 5:
-        choice_for_delete = ("You Want to delete the data")
-        if choice_for_delete == 'y' or choice_for_delete == 'Y':
-            demanding_information_to_delet = ("Enter the Name you want to remove")
-            for workerobj in Worker.worker_list:
-                if demanding_information_to_delet == workerobj.name:
-                    del Workerobj
-                else:
-                    print("data is not found to delete")
-
-            choice_for_exit = input("IF YOU WANT TO EXIT THEN PRESS YES AND IF YOU DO NOT WANT TO EXIT PRESS NO")
-            if choice_for_exit == 'yes' or choice_for_exit == 'YES':
+            choice_for_more_update = input("Do you Want to update More Information [y/n]\n")
+            if choice_for_more_update == 'Y' or choice_for_more_update == 'y':
                 break
+
+
+
+    elif choice_of_user == 4:
+        while True:
+            w1=Worker()
+            w1.Delete_Worker_Information()
+            choice_for_more_Delete = input("Do You want to delete more Information [y/n]\n")
+            if choice_for_more_Delete == 'y' or choice_for_more_Delete == 'Y':
+                break
+
+    choice_for_exit = input("IF YOU WANT TO EXIT THEN PRESS YES AND IF YOU DO NOT WANT TO EXIT PRESS NO")
+    if choice_for_exit == 'yes' or choice_for_exit == 'YES':
+        break
 
 
 
